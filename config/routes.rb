b3bc5cced34794d6ea5805bacd27711cc580a148
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-09-24 04:43:05 +0000

    Add cable route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  #mount ActionCable.server, at: '/cable'
+  mount ActionCable.server, at: '/cable'
 end
